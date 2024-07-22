588421cd1dbdcaf9a254c5d2b1d7185f325a2eb5
badmagick329 <uzairfarooqi@gmail.com>
2024-07-22 13:52:45 +0100

    refactor: bullet spawner

--- src/js/components/bullet-spawner.js
+++ src/js/components/bullet-spawner.js
@@ -1,9 +1,15 @@
 import RAPIER from '@dimforge/rapier3d-compat'
 import * as THREE from 'three'
 import { Component } from '../ecs'
+import { GameBody } from '../game'
 
-const INTERVAL = 150
-const MAX_TIME = 20000
+const config = {
+  intervalBetweenShots: 150,
+  maxTimeAlive: 20000,
+  maxTravel: 50,
+  triggerKey: 'space',
+  bulletSpeed: 24.0,
+}
 
 export class BulletSpawner extends Component {
   /**
@@ -16,12 +22,18 @@ export class BulletSpawner extends Component {
     this._playerBody = playerBody
     this._scene = scene
     this._world = world
-    this._bulletSpeed = 24.0
     this._bullets = []
-    this._readyToShoot = true
     this._lastShot = null
-    this._interval = INTERVAL
-    this._max_time = MAX_TIME
+    this._config = config
+  }
+
+  update(time, delta) {
+    this._handleShoot(time)
+
+    for (let i = 0; i < this._bullets.length; i++) {
+      this._bullets[i].bullet.sync()
+      this._handleDisposal(this._bullets[i], this._bullets[i].timeAlive, i)
+    }
   }
 
   _shootBullet(direction) {
@@ -32,79 +44,83 @@ export class BulletSpawner extends Component {
     const bulletMesh = new THREE.Mesh(bulletGeometry, bulletMaterial)
     this._scene.add(bulletMesh)
 
-    const bulletBody = this._createBulletBody(position, direction)
-    this._bullets.push({ mesh: bulletMesh, body: bulletBody, timeAlive: 0.0 })
+    const bulletBodyAndCollider = this._createBulletBodyAndCollider(
+      position,
+      direction
+    )
+    const bullet = new GameBody(
+      bulletMesh,
+      bulletBodyAndCollider.rigidBody,
+      bulletBodyAndCollider.collider
+    )
+    // TODO: Calc max travel based on player's current position
+    // Might need to change how shootBullet is being called too
+    this._bullets.push({
+      bullet,
+      timeAlive: 0.0,
+      maxTravel: this._config.maxTravel,
+    })
   }
 
-  _createBulletBody(position, direction) {
-    let desc = RAPIER.RigidBodyDesc.dynamic()
+  _createBulletBodyAndCollider(position, direction) {
+    const desc = RAPIER.RigidBodyDesc.dynamic()
       .setTranslation(position.x, position.y, position.z)
       .setLinvel(
-        direction.x * this._bulletSpeed,
-        direction.y * this._bulletSpeed,
-        direction.z * this._bulletSpeed
+        direction.x * this._config.bulletSpeed,
+        direction.y * this._config.bulletSpeed,
+        direction.z * this._config.bulletSpeed
       )
       .setGravityScale(0.0)
       .setAdditionalMass(1000)
       .setCcdEnabled(true)
-    let body = this._world.createRigidBody(desc)
+    const rigidBody = this._world.createRigidBody(desc)
 
-    let colDesc = RAPIER.ColliderDesc.ball(0.8).setFriction(0.5)
-    this._world.createCollider(colDesc, body)
+    const colDesc = RAPIER.ColliderDesc.ball(0.8).setFriction(0.5)
+    const collider = this._world.createCollider(colDesc, rigidBody)
 
-    return body
+    return { rigidBody, collider }
   }
 
-  handleShoot() {
-    this._shootBullet({ x: 0, y: 0, z: 1 })
-  }
-
-  update(time, delta) {
-    if (this._lastShot === null || time - this._lastShot > this._interval) {
+  _handleShoot(time) {
+    if (
+      this._lastShot === null ||
+      time - this._lastShot > this._config.intervalBetweenShots
+    ) {
       for (const [key, active] of Object.entries(
         this.getComponent('InputController')._keys
       )) {
-        if (key === 'space' && active) {
-          this.handleShoot()
+        if (key === this._config.triggerKey && active) {
+          this._shootBullet({ x: 0, y: 0, z: 1 })
           this._lastShot = time
         }
       }
     }
+  }
 
-    for (let i = 0; i < this._bullets.length; i++) {
-      let position = this._bullets[i].body.translation()
-      this._bullets[i].mesh.position.set(position.x, position.y, position.z)
-
-      let rotation = this._bullets[i].body.rotation()
-      this._bullets[i].mesh.quaternion.set(
-        rotation.x,
-        rotation.y,
-        rotation.z,
-        rotation.w
-      )
-
-      this._bullets[i].timeAlive += delta
-
-      if (
-        position.z > 200 ||
-        position.z < -200 ||
-        position.x > 200 ||
-        position.x < -200 ||
-        position.y > 200 ||
-        position.y < -200
-      ) {
-        this.disposeBullet(i)
-      } else if (this._bullets[i].timeAlive > this._max_time) {
-        this.disposeBullet(i)
-      }
+  /**
+   * @param {GameBody} bullet
+   * @param {number} index
+   */
+  _handleDisposal(bullet, index) {
+    const position = bullet.bullet.rigidBody.translation()
+    if (
+      position.z > bullet.maxTravel ||
+      position.z < -bullet.maxTravel ||
+      position.x > bullet.maxTravel ||
+      position.x < -bullet.maxTravel ||
+      position.y > bullet.maxTravel ||
+      position.y < -bullet.maxTravel
+    ) {
+      this._disposeBullet(index)
+    } else if (bullet.timeAlive > this._config.maxTimeAlive) {
+      this._disposeBullet(index)
     }
   }
 
-  disposeBullet(idx) {
-    // console.log('disposing', idx);
+  _disposeBullet(idx) {
     // TODO: Ensure everything is actually getting disposed
-    this._scene.remove(this._bullets[idx].mesh)
-    this._world.removeRigidBody(this._bullets[idx].body)
+    this._scene.remove(this._bullets[idx].bullet.mesh)
+    this._world.removeRigidBody(this._bullets[idx].bullet.rigidBody)
     this._bullets.splice(idx, 1)
   }
 }
